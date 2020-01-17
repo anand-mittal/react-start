16857699740ecc456688e880c354e1e048f046ff
anand-mittal <anand.gwl@gmail.com>
2020-01-17 20:27:34 +0530

    es-lint n redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-  useParams,
-  useRouteMatch
-} from "react-router-dom";
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+
+import Login from './Login';
+import Home from './Home';
+import Dashboard from './Dashboard';
+import Footer from './shared/Footer';
+import Header from './shared/Header';
+
+const routes = (
+  <Router>
+    <Route exact path="/" component={Home}></Route>
+    <Route path="/login" component={Login}></Route>
+    <Route path="/dashboard" component={Dashboard}></Route>
+  </Router>
+);
 
 const App = () => (
-  <pre>
-    <div>Hello world !!</div>
-  </pre>
-)
+  <div className="container">
+    <Header></Header>
+    {routes}
+    <Footer></Footer>
+  </div>
+);
 
 export default App;
